c058c43d40869970d64be48f3665ed9f2c5a5224
Itay <69512729+spicyshotz@users.noreply.github.com>
2025-06-11 20:59:19 +0300

    Refined vanity responses and request verification
    now i make sure that the request has a valid URI structure and added BadRequest responses in cases where another vanity url is in use for the requested full URL, and cases where the vanity URL already exists for another URL
    
    YIPEE!!!!

--- ShorterThanAverage/api.cs
+++ ShorterThanAverage/api.cs
@@ -20,21 +20,37 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+
 // shortening endpoint
-app.MapPost("/api/shorten/", async (string request, string? vanity, UrlDatabase db) =>
+app.MapPost("/api/shorten/", async (HttpContext context, string request, string? vanity, UrlDatabase db) =>
 {
     if (!string.IsNullOrEmpty(vanity) && !Regex.IsMatch(vanity, @"^[a-zA-Z0-9]+$"))
     {
         return Results.BadRequest("Vanity URL must contain only a-z, A-Z, or 0-9 characters.");
     }
+    
+    if (!Uri.IsWellFormedUriString(request, UriKind.Absolute))
+    {
+        return Results.BadRequest("Invalid URL format. Please provide a valid URL.");
+    }
+
+    var host = context.Request.Host.Value;
+    var scheme = context.Request.Scheme;
+    var shortUrl = string.Empty;
 
     var dbResult = await db.CheckUrlExistanceAsync(request);
     if (dbResult.url is not null)
     {
-        return Results.Ok(dbResult.shortUrl);
+        if (!string.IsNullOrWhiteSpace(vanity) && dbResult.shortUrl != vanity)
+        {
+            return Results.BadRequest("Vanity Failed: Shortened URL already exists for this full URL. Please try shortening a different URL.");
+        }
+        shortUrl = $"{scheme}://{host}/{dbResult.shortUrl}";
+        return Results.Ok(shortUrl);
     }
 
     URL UrlObject = new URL(request, vanity); // vanity can be null, as i check it in URL class and generate a shorteneted URL if a vanity one is not provided.
+
     dbResult = await db.InsertUrlAsync(UrlObject.FullUrl, UrlObject.ShortenedUrl);
     while (dbResult.url != UrlObject.FullUrl)
     {
@@ -42,13 +58,15 @@ app.MapPost("/api/shorten/", async (string request, string? vanity, UrlDatabase 
         {
             return Results.BadRequest("Vanity URL already exists. Please choose a different one.");
         }
-        
+
         UrlObject.RegenerateShortenedUrl();
         dbResult = await db.InsertUrlAsync(UrlObject.FullUrl, UrlObject.ShortenedUrl);
     }
-    return Results.Ok(UrlObject.ShortenedUrl);
+    shortUrl = $"{scheme}://{host}/{UrlObject.ShortenedUrl}";
+    return Results.Ok(shortUrl);
 })
 .WithName("ShortenURL");
+
 
 // redirecting to full URL endpoint
 app.MapGet("{short_code}", async (string short_code, UrlDatabase db) =>
